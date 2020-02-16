65671ce7653ff652adfc5866b688e479811bd9e2
William Ruys <wlruys@login1.maverick2.tacc.utexas.edu>
2020-02-16 02:31:14 -0600

    Removing build directory from repo

--- prknn/kernels/gpu/impl/primitives.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PRIMITIVES_GPU_HPP
-#define PRIMITIVES_GPU_HPP
-
-#include<stdio.h>
-
-/* The function to be wrapped in Cython */
-void vector_add(float *out, float *a, float *b, size_t n);
-
-#endif //PRIMITIVES_GPU_HPP
--- prknn/kernels/gpu/primitives.pxd
+++
@@ -1,3 +0,0 @@
-
-cdef extern from "impl/primitives.hpp" nogil:
-    cdef void vector_add(float *out, float *a, float* b, size_t n)
