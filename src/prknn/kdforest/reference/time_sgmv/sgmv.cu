10ec1c7aa5f35df0de8f13f4f3fa3ab7177f241f
GalvinYang97 <paul_yhr@outlook.com>
2020-03-09 09:31:13 -0500

    add sgmv timer

---
+++ src/prknn/kdforest/reference/time_sgmv/sgmv.cu
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stdio.h>
+#include <cublasXt.h>
+#include <curand.h>
+
+#include "timer.hpp"
+
+#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
+    printf("Error at %s:%d\n",__FILE__,__LINE__);\
+    exit(EXIT_FAILURE);}} while(0)
+#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
+    printf("Error at %s:%d\n",__FILE__,__LINE__);\
+    exit(EXIT_FAILURE);}} while(0)
+
+void createRandomMatrix(curandGenerator_t &gen, int n, int d, void **matrix_gpu)
+{
+    CUDA_CALL(cudaMalloc(matrix_gpu, n*d*sizeof(float)));
+    CURAND_CALL(curandGenerateUniform(gen,(float *) *matrix_gpu,n*d));
+    
+}
+
+int main()
+{
+    int d = 100;
+    int n = 2e6;
+    float *matrix_gpu, *vector_gpu;
+    float *matrix = (float *) calloc(n*d, sizeof(float));
+    float *vector = (float *) malloc(d*sizeof(float));
+    Timer timer = Timer();
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    curandGenerator_t gen;
+
+    /* Create pseudo-random number generator */
+    CURAND_CALL(curandCreateGenerator(&gen,CURAND_RNG_PSEUDO_DEFAULT));
+
+    createRandomMatrix(gen,n,d,(void **) &matrix_gpu);
+    createRandomMatrix(gen,d,1,(void **) &vector_gpu);
+    
+    CUDA_CALL(cublasSetMatrix(n,d,sizeof(float),matrix,n,matrix_gpu,n));
+    CUDA_CALL(cublasSetVector(d,sizeof(float),vector,1,vector_gpu,1));
+
+    float *result = (float *) calloc(n,sizeof(float));
+    float *result_gpu;
+    CUDA_CALL(cudaMalloc((void**) &result_gpu, n*sizeof(float)));
+    CUDA_CALL(cublasSetVector(n,sizeof(float),result,1,result_gpu,1));
+    float alpha = 1.;
+    float beta = 1.;
+
+    timer.start();
+    CUDA_CALL(cublasSgemv(handle, CUBLAS_OP_N,
+        n, d,
+        &alpha,
+        matrix_gpu, n,
+        vector_gpu, 1,
+        &beta,
+        result_gpu, 1));
+    timer.stop();
+    timer.show_elapsed_time();
+
+    CUDA_CALL(cudaFree(matrix_gpu));
+    CUDA_CALL(cudaFree(vector_gpu));
+    CUDA_CALL(cudaFree(result_gpu));
+    free(matrix);
+    free(vector);
+    free(result);
+
+    return 0;
+}
