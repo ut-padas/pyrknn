68134ab3b24676f1bf48b72737d9256abac534b2
GalvinYang97 <paul_yhr@outlook.com>
2020-03-09 10:13:55 -0500

    fix sgmv

--- src/prknn/kdforest/reference/time_sgmv/sgmv.cu
+++ src/prknn/kdforest/reference/time_sgmv/sgmv.cu
@@ -21,31 +21,32 @@ void createRandomMatrix(curandGenerator_t &gen, int n, int d, void **matrix_gpu)
 
 int main()
 {
-    int d = 100;
-    float *matrix_gpu, *vector_gpu;
-    float *vector = (float *) malloc(d*sizeof(float));
-    Timer timer = Timer();
-
     cublasHandle_t handle;
     cublasCreate(&handle);
     curandGenerator_t gen;
-
     /* Create pseudo-random number generator */
     CURAND_CALL(curandCreateGenerator(&gen,CURAND_RNG_PSEUDO_DEFAULT));
-    createRandomMatrix(gen,d,1,(void **) &vector_gpu);
 
+    int d = 100;
+    int n;
+    float *matrix, *matrix_gpu, *vector_gpu;
+    float *vector = (float *) malloc(d*sizeof(float));
+    createRandomMatrix(gen,d,1,(void **) &vector_gpu);
+    CUDA_CALL(cublasSetVector(d,sizeof(float),vector,1,vector_gpu,1));
+
+    Timer timer = Timer();
+   
     float alpha = 1.;
     float beta = 1.;
 
     for (int i = 1; i < 10; i++)
     {
-        n = i * 1e6;
+        n = i * 1e5;
         matrix = (float *) calloc(n*d, sizeof(float));
         createRandomMatrix(gen,n,d,(void **) &matrix_gpu);
         
         CUDA_CALL(cublasSetMatrix(n,d,sizeof(float),matrix,n,matrix_gpu,n));
-        CUDA_CALL(cublasSetVector(d,sizeof(float),vector,1,vector_gpu,1));
-
+        
         float *result = (float *) calloc(n,sizeof(float));
         float *result_gpu;
         CUDA_CALL(cudaMalloc((void**) &result_gpu, n*sizeof(float)));
@@ -59,14 +60,19 @@ int main()
             vector_gpu, 1,
             &beta,
             result_gpu, 1));
+        cudaDeviceSynchronize();
         timer.stop();
         timer.show_elapsed_time();
+
+        CUDA_CALL(cublasGetVector(n,sizeof(float),result_gpu,1,result,1));
 
         CUDA_CALL(cudaFree(matrix_gpu));
         CUDA_CALL(cudaFree(result_gpu));
         free(matrix);
         free(result);
     }
+    CUDA_CALL(cublasDestroy(handle));
+    CURAND_CALL(curandDestroyGenerator(gen));
     CUDA_CALL(cudaFree(vector_gpu));
     free(vector);
     return 0;
