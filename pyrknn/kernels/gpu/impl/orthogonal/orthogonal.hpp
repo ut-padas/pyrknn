d691c99e3eb9467d3a211845de2bd78e3a71888e
William L. Ruys <will.ruys@gmail.com>
2021-04-02 17:10:46 -0500

    Delete pyrknn directory

--- pyrknn/kernels/gpu/impl/orthogonal/orthogonal.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ORTHOGONAL_HPP
-#define ORTHOGONAL_HPP
-
-#include <thrust/device_vector.h>
-
-void orthogonal(thrust::device_vector<float>&, int, int);
-
-#endif
--- pyrknn/kernels/gpu/impl/sparse/spknn.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SPKNN_HPP
-#define SPKNN_HPP
-
-
-void spknn(int*, int*, int*, float*, int, int, int, int, int, int*, float*, int, int, int, int);
-
-#endif
