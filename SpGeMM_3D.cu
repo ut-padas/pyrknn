dcefecff2faf6c51379aae2ec190a58bb82ad17d
Ali Ghafouri Pourkermani <ghafouri@login2.frontera.tacc.utexas.edu>
2021-04-01 22:11:13 -0500

    craete final file

--- SpGeMM_3D.cu
+++ SpGeMM_3D.cu
@@ -11,68 +11,76 @@
 
 
 
-__global__ void compute_dist(int* I, int* J, float* V, float* D, int m, int log_max_nnz, int Z){ 
+__global__ void compute_dist(int* I, int* J, float* V, float* D, int m, int* nnz_vec, int log_max_nnz, int Z){ 
 
     int i = threadIdx.x + blockDim.x*blockIdx.x; 
     int j = blockIdx.y ;
     int z = blockIdx.z ;
 
     if (i>= m || j >= m || z >= Z) return;
-
+    if (i == j) return;
     
 
     int ind0_i = I[z*(m+1) + i];
     int ind1_i = I[z*(m+1) + i+1];
     int ind0_j = I[z*(m+1) + j];
     int ind1_j = I[z*(m+1) + j+1];
-
-
+    int nnz = nnz_vec[z];
 
 
     int nnz_i = ind1_i - ind0_i;
     int nnz_j = ind1_j - ind0_j;
-    
+    int ind_out = i*m + j + z*pow(m, 2);
+    //float *val_out;
+    //val_out = &D[ind_out];
+    //printf("run 1 \n ");
 
     if (nnz_j == 0 || nnz_i ==0) return;
 
+    
+    __shared__ int sj[1000];
+    __shared__ float v_j[1000];
 
-    __shared__ int sj[2000];
-    //int *sj; 
-    int *si;
-    si = (int *)malloc(sizeof(int)*nnz_i);
-    for (int n_i=0; n_i < nnz_i; n_i++) si[n_i] = J[n_i];
+    int si[1000];
+    float v_i[1000];
+    //si = (int *)malloc(sizeof(int)*nnz_i);
+    
+
+    //printf("run 1 \n"); 
+    for (int n_i=0; n_i < nnz_i; n_i++) si[n_i] = J[ind0_i + n_i + z*nnz];
+    for (int n_i=0; n_i < nnz_i; n_i++) v_i[n_i] = V[ind0_i + n_i + z*nnz]; 
+        
     //sj = (int *)malloc(sizeof(int)*nnz_j);
-    for (int n_j=0; n_j < nnz_j; n_j++) sj[n_j] = J[n_j];
-
-    //printf("arrays si : \n")
-    /*
-    if (i==1 && j==2){ 
-    for (int n_i=ind0_i; n_i < ind1_i; n_i++) printf("i = %d , si[%d] = %d \n ",i, n_i, si[n_i]);
-    printf("\n\n");
-    for (int n_j=ind0_j; n_j < ind1_j; n_j++) printf("j = %d , sj[%d] = %d \n ", j, n_j, sj[n_j]);
-    }
-    */
+    for (int n_j=0; n_j < nnz_j; n_j++) v_j[n_j] = V[ind0_j + n_j + z*nnz];
+    sj[i] = J[ind0_j + i + z*nnz];
+    //v_j[i] = V[ind0_j + j + z*nnz];
+    
+    __syncthreads();
+    
+    //printf("run 2 \n ");
     //sj = J[ind0_j:ind1_j];
     //si = J[ind0_i:ind1_i];
     float norm_ij = 0;
-
-    for (int n_i=0; n_i < nnz_i; n_i++) norm_ij += pow(V[ind0_i + n_i], 2);
-    for (int n_j=0; n_j < nnz_j; n_j++) norm_ij += pow(V[ind0_j + n_j], 2);
-
+    
+    for (int n_i=0; n_i < nnz_i; n_i++) norm_ij += pow(V[ind0_i + n_i + z*nnz], 2);
+    for (int n_j=0; n_j < nnz_j; n_j++) norm_ij += pow(V[ind0_j + n_j + z*nnz], 2);
+    //printf("run 3 \n ");
     float c_tmp = 0; 
     //int tmp = log2f(nnz_j);
-
-    for (int pos_k=0; pos_k<nnz_i;pos_k++){
-
-        int k = si[pos_k];
-        int ret = 0;
-        int testInd = 0;
-        int tmp_0,tmp_1;
+    int ret, testInd, tmp_0, tmp_1, k, l, ind_jk;
+        
+    
+    for (int pos_k=0; pos_k<nnz_i;pos_k++){       
+        //int k = J[pos_k+ind0_i + z*nnz];
+        k = si[pos_k];
+        ret=0; 
+        testInd = 0;
+        
         // Binary search 
-        for (int l=log_max_nnz; l > 0; l--){
+        for (l=log_max_nnz; l > 0; l--){
             if (pow(2, l) > nnz_j) continue;
-            int tmp_0 = ret+pow(2, l);
-            int tmp_1 = nnz_j-1;
+            tmp_0 = ret+pow(2, l);
+            tmp_1 = nnz_j-1;
             testInd = min(tmp_0, tmp_1);
             ret = (sj[testInd] <= k) ? testInd : ret ;
         }
@@ -80,16 +88,16 @@ __global__ void compute_dist(int* I, int* J, float* V, float* D, int m, int log_
         tmp_1 = nnz_j-1;
         testInd = min(tmp_0, tmp_1);
         ret = (sj[testInd] <= k) ? testInd : ret;
-        int ind_jk = (sj[ret] == k) ? ret : -1;
-
-        c_tmp += (ind_jk != -1) ? V[ind0_i + pos_k]*V[ind0_j+ind_jk] : 0;
+        ind_jk = (sj[ret] == k) ? ret : -1;
+        //if (i==1 && j==2) printf("i = %d, j = %d , k = %d, pos_k = %d, ind_jk = %d , v_i = %.2f, v_j = %.2f \n",i,j,k,pos_k, ind_jk, v_i[pos_k], v_j[ind_jk]);
+        //c_tmp += (ind_jk != -1) ? V[ind0_i + pos_k + z*nnz]*V[ind0_j+ind_jk + z*nnz] : 0;
+        c_tmp += (ind_jk != -1) ? v_i[pos_k]*v_j[ind_jk] : 0;
+        
         //c_tmp += c;
     }
-
-    //c_tmp = max(-2*c_tmp + norm_ij, 0)
-    int ind = i*m+j+z*pow(m,2);
-    D[ind] = c_tmp;
-
+    //c_tmp = max(-2*c_tmp + norm_ij, 0);
+    //printf("D[%d, %d] = %.2f for z = %d and ind = %d \n", i, j, c_tmp, ind_out, z); 
+    D[ind_out] = c_tmp;
 }
 
 
@@ -116,69 +124,102 @@ int main(int argc, char **argv)
     int *h_J, *d_J;
     int *h_I, *d_I;
     float *h_D, *d_D; 
+    int *h_nnz_all, *d_nnz_all; 
     cudaEvent_t t0; 
     cudaEvent_t t1;
 
 
 
-    m = 300;
+    m = 200;
     d = 10000;
-    nnzperrow = 50;
+    nnzperrow = 400;
     Z = 1;
     nnz = nnzperrow*m;
-    int max_nnz = d;
+    int max_nnz = 2000;
     int log_max_nnz = log2(max_nnz);
 
-    h_V = (float *)malloc(sizeof(float)*nnzperrow*m);
-    h_J = (int *)malloc(sizeof(int)*nnzperrow*m);
-    h_I = (int *)malloc(sizeof(int)*(m+1));
+    h_V = (float *)malloc(sizeof(float)*nnz*Z);
+    h_J = (int *)malloc(sizeof(int)*nnz*Z);
+    h_I = (int *)malloc(sizeof(int)*(m+1)*Z);
+    h_nnz_all = (int *)malloc(sizeof(int) *Z);
 
     // generate random data 
 
-    h_I[0] = 0;
     
-    for (int i=0; i < m; i++){
-        h_I[i+1] = h_I[i] + nnzperrow;
-        int *tmp;
+    
+    for (int z=0; z < Z; z++){ 
+        h_I[0+z*(m+1)] = 0;
+        h_nnz_all[z] = nnz;
+        for (int i=0; i < m; i++){
+        h_I[i+1+z*(m+1)] = h_I[i+z*(m+1)] + nnzperrow;
+        
         //tmp_col = (int *)malloc(sizeof(int)*nnzperrow);
         //tmp_val = (float *)malloc(sizeof(float)*nnzperrow);
         for (int j=0; j < nnzperrow; j++){
-            int ind = h_I[i]+j;
+            int ind = h_I[i]+j + z*(nnz);
             int val = rand()%d;
             while (val == h_J[ind-1]) val = rand()%d;
             h_J[ind] = val;
-            h_V[ind] = (float)rand()/(float)(RAND_MAX);
+            h_V[ind] = j;
+            //(float)rand()/(float)(RAND_MAX); 
+            //
         }    
-        std::sort(h_J+i*nnzperrow, h_J+(i+1)*nnzperrow);
+        std::sort(h_J+i*nnzperrow+ z*nnz, h_J+(i+1)*nnzperrow+z*nnz);
     }
+    }
+    /*
+    for (int z=0; z < Z; z++){
+        printf("\n z = %d \n\n", z);
+    printf("row pointer : \n ");
+    for (int i=0; i < m+1; i++) {
+        printf(" %d ", h_I[i+z*(m+1)]);
+    }
+    printf("\n \n ");
+    printf("col indices : \n "); 
+    for (int i=0; i < nnz; i++) {
+        printf(" %d ", h_J[i+z*(nnz)]);
+    }
+    printf("\n \n ");
+    printf("vals : \n "); 
+    for (int i=0; i < nnz; i++) {
+        printf(" %.4f ", h_V[i+z*nnz]);
+    }
+    }*/
+    
+
+
 
     h_D = (float *)malloc(sizeof(float)*m*m*Z);
 
     // prop of execution
-    int B =  (256 > m) ? m: 256;
+    int B =  (32 > m) ? m: 32;
     dim3 dimBlock(B,1,1);	// 512 threads per thread-block
-	dim3 dimGrid((m+B-1)/B, m, Z); // Enough thread-blocks to cover N
+    int threadblock = (m+B-1)/B;
+	  dim3 dimGrid(threadblock, m, Z); // Enough thread-blocks to cover N
 
 
     // gen device arrays
-    checkCudaErrors(cudaMalloc((void **) &d_I, sizeof(int)*(m+1)));
-    checkCudaErrors(cudaMalloc((void **) &d_J, sizeof(int)*nnz));
-    checkCudaErrors(cudaMalloc((void **) &d_V, sizeof(float)*nnz));
-    checkCudaErrors(cudaMalloc((void **) &d_D, sizeof(float)*m*m));
+    checkCudaErrors(cudaMalloc((void **) &d_I, sizeof(int)*(m+1)*Z));
+    checkCudaErrors(cudaMalloc((void **) &d_J, sizeof(int)*nnz*Z));
+    checkCudaErrors(cudaMalloc((void **) &d_V, sizeof(float)*nnz*Z));
+    checkCudaErrors(cudaMalloc((void **) &d_nnz_all, sizeof(int)*Z));
+    checkCudaErrors(cudaMalloc((void **) &d_D, sizeof(float)*m*m*Z));
 
     // copy to device 
 
-    checkCudaErrors(cudaMemcpy(d_I, h_I, sizeof(int)*(m+1), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_J, h_J, sizeof(int)*(nnz), cudaMemcpyHostToDevice));
-    checkCudaErrors(cudaMemcpy(d_V, h_V, sizeof(float)*(nnz), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_I, h_I, sizeof(int)*(m+1)*Z, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_J, h_J, sizeof(int)*(nnz)*Z, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_nnz_all, h_nnz_all, sizeof(int)*Z, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_V, h_V, sizeof(float)*(nnz)*Z, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_D, h_D, sizeof(float)*m*m*Z, cudaMemcpyHostToDevice));
 
-    checkCudaErrors(cudaMemset(d_D, 0, sizeof(float) * m * m));
+    //checkCudaErrors(cudaMemset(d_D, 0, sizeof(float) * m * m * Z));
 
     checkCudaErrors(cudaEventCreate(&t0));
     checkCudaErrors(cudaEventCreate(&t1));
 
     checkCudaErrors(cudaEventRecord(t0, 0));
-    compute_dist<<<dimGrid, dimBlock>>>(d_I, d_J, d_V, d_D, m, log_max_nnz, Z);
+    compute_dist<<<dimGrid, dimBlock>>>(d_I, d_J, d_V, d_D, m, d_nnz_all, log_max_nnz, Z);
     
     checkCudaErrors(cudaEventRecord(t1, 0));
     checkCudaErrors(cudaEventSynchronize(t1));
@@ -186,41 +227,48 @@ int main(int argc, char **argv)
     checkCudaErrors(cudaEventElapsedTime(&del_t1, t0, t1));
 
 
-    checkCudaErrors(cudaMemcpy(h_D, d_D, sizeof(float)*m*m, cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(h_D, d_D, sizeof(float)*m*m*Z, cudaMemcpyDeviceToHost));
     printf("\n Elapsed time (ms) : %.4f \n ", del_t1);
     /*
+    for (int z=0; z < Z; z++){
+        printf(" z = %d \n\n", z);
     printf("row pointer : \n ");
     for (int i=0; i < m+1; i++) {
-        printf(" %d ", h_I[i]);
+        printf(" %d ", h_I[i+z*(m+1)]);
     }
     printf("\n \n ");
     printf("col indices : \n "); 
     for (int i=0; i < nnz; i++) {
-        printf(" %d ", h_J[i]);
+        printf(" %d ", h_J[i+z*(nnz)]);
     }
     printf("\n \n ");
     printf("vals : \n "); 
     for (int i=0; i < nnz; i++) {
-        printf(" %.4f ", h_V[i]);
+        printf(" %.4f ", h_V[i+z*nnz]);
     }
     printf("\n \n ");
     printf("out  : \n ");
     for (int i=0; i< m; i++){
         printf("\n [ ");
         for (int j=0; j < m; j++){
-            printf(" %.4f ", h_D[i*m + j]);
+            printf(" %.4f ", h_D[i*m + j + z*m*m]);
         }
         printf(" ]");
     }
-    printf("\n\n");
+    
+    printf("\n");
+    }
     */
+    printf("\n\n");
     checkCudaErrors(cudaFree(d_I));
     checkCudaErrors(cudaFree(d_J));
     checkCudaErrors(cudaFree(d_V));
     checkCudaErrors(cudaFree(d_D));
+    checkCudaErrors(cudaFree(d_nnz_all));
     checkCudaErrors(cudaEventDestroy(t0));
     checkCudaErrors(cudaEventDestroy(t1));
     free(h_I);
+    free(h_nnz_all);
     free(h_J);
     free(h_V);
     free(h_D);
---
+++ SpGeMM_3D_mod.py
@@ -0,0 +1,276 @@
+
+import numpy as np 
+
+from scipy import sparse 
+import time
+from numba import cuda, float32, int16, int32, int64, gdb_init, types, typeof
+import math
+import numba
+import time
+from scipy import sparse
+import random
+  
+
+
+
+
+
+
+
+
+@cuda.jit('void(int32[:], int32[:], float32[:], float32[:], int32, int32, int32, int32)') 
+def SpGeMM_3D(I, J, V, D, m, nnzperrow, max_nnz, Z):
+  
+
+  i = cuda.threadIdx.x + cuda.blockDim.x*cuda.blockIdx.x
+  j = cuda.blockIdx.y
+  z = cuda.blockIdx.z
+ 
+  if i>= m or j>= m or z>= Z: return
+  
+
+  
+
+  ind0_i=I[i + z*(m+1)]
+  ind1_i=I[i+1 + z*(m+1)]
+
+  ind0_j=I[j + z*(m+1)]
+  ind1_j=I[j+1 + z*(m+1)]
+
+
+  nnz_j = ind1_j - ind0_j
+  nnz_i = ind1_i - ind0_i
+
+
+  if nnz_j==0 or nnz_i == 0 : return
+
+  
+
+
+  #sj =J[ind0_j:ind1_j]
+
+  sj = cuda.shared.array(shape=(2000),dtype=int32)
+  for l in range(nnz_j):
+    sj[l] = J[ind0_j+l]
+ 
+ 
+  si = J[ind0_i:ind1_i]
+  
+
+  
+  norm_ij = 0  
+  v_i = V[ind0_i:ind1_i]
+  v_j = V[ind0_j:ind1_j]
+  
+  for n_i in range(nnz_i):
+    norm_ij += v_i[n_i]**2
+  for n_j in range(nnz_j):
+    norm_ij += v_j[n_j]**2
+
+
+  
+  c_tmp = 0
+  #log_n_true = math.floor(math.log(nnz_j)/math.log(2)) 
+  log_n = math.floor(math.log(max_nnz)/math.log(2)) 
+  log_n = math.floor(math.log(nnz_j)/math.log(2)) 
+  #log_n = math.log(nnz_j)/math.log(2)
+  for pos_k in range(0, ind1_i-ind0_i):
+
+    k = si[pos_k]
+     
+    # Binary_search
+    ret = 0
+    testInd = 0
+    
+    for l in range(log_n, 0, -1):
+      #if l>log_n_true: continue
+      testInd = min(ret + 2**l, nnz_j-1)
+      ret = testInd if sj[testInd]<= k else ret 
+    testInd = min(ret+1, nnz_j-1)
+    ret = testInd if sj[testInd]<=k else ret
+    ind_jk = ret if sj[ret] == k else -1
+
+    c = v_i[pos_k]*v_j[ind_jk] if ind_jk != -1 else 0
+    #if i == 0 and j==1: print(i, j, k, ind_jk, c)
+    c_tmp += c
+
+  #c_tmp = max(-2*c_tmp + norm_ij, 0)
+  #print(i,j)
+  ind = m*i + j + z*(m**2)
+  D[ind] = c_tmp
+
+
+
+
+def rec(I, J, V, m, d):
+  A = np.zeros((m,d))
+  for i in range(m):
+    if I[i] != I[i+1]:
+      nnz = I[i+1] - I[i]
+      for j in range(nnz):
+        col = J[I[i]+j]
+        A[i, col] = V[I[i]+j]
+ 
+  OUT = np.matmul(A, A.transpose())
+  return A, OUT
+
+
+
+
+
+
+
+
+def gen_SpData_2D(m, d, nnzperrow, Z):
+  
+  nnz = nnzperrow*m
+  I = np.zeros(((m+1)*Z), dtype = np.int32)
+  J = np.zeros((nnz*Z), dtype = np.int32)
+  V = np.zeros((nnz*Z), dtype = np.float32)
+
+
+  for z in range(Z):
+    I[0 + z*(m+1)] = 0
+    for i in range(m):
+      I[i+1+z*(m+1)] = I[i+z*(m+1)] + nnzperrow
+      for j in range(nnzperrow):
+        
+        ind = I[i] + j + z*nnz
+        val = random.randrange(0, d, 1) 
+        while val == J[ind-1]:
+          val = random.randrange(0, d, 1) 
+        J[ind] = val
+        V[ind] = j
+      ind0 = i*nnzperrow + z*nnz
+      ind1 = (i+1)*nnzperrow + z*nnz
+         
+      J[ind0:ind1] = np.sort(J[ind0:ind1])
+  
+       
+
+  #dens = nnz/(d*m)
+  #I = np.zeros((m+1,Z), dtype=np.int32)
+  #J = np.zeros((nnz, Z), dtype=np.int32)
+  #V = np.zeros((nnz, Z), dtype=np.float32)
+  
+  
+  
+
+
+  '''
+  for z in range(Z):
+    S = sparse.random(m,d, density= dens, format='csr')
+    I[:, z] = np.array(list(S.indptr), dtype = np.int32)
+    J[:, z] = np.array(list(S.indices), dtype = np.int32)
+    V[:, z] = np.array(list(S.data), dtype = np.float32)
+  '''
+
+  return I, J, V
+
+
+
+def main():
+  er = 0
+  d = 5
+  m = 5
+  nnzperrow = 3
+  nnz = nnzperrow*m
+  max_nnz = 5   # max nnz per row
+  L = 1
+  Z = 1		  # num of simoltaneous AA^T
+
+  cuda.select_device(0) 
+  tot_t1 = 0
+  tot_t2 = 0
+  tot_t = 0
+  tot_ti = 0
+  B = min(256, m)
+  blockpergrid = (m + B-1)//B
+  blockpergrid = max(1, blockpergrid)
+  blockdim = B, 1, 1
+  griddim = blockpergrid, m, Z
+  print(blockpergrid)
+  print(B)
+  er = 0
+  print('rows of D ', m)
+  for l in range(L):
+    I1, J1, V1 = gen_SpData_2D(m, d, nnzperrow, Z)
+
+    print('row pointer ')
+    print(I1)
+    print('col indices')
+    print(J1)
+    print('val indices')
+    print(V1)
+		 
+    D = np.zeros((m**2*Z), dtype = np.float32)
+    d_I = cuda.to_device(I1)
+    d_J = cuda.to_device(J1)
+    d_V = cuda.to_device(V1)
+    d_D = cuda.to_device(D)
+    t0 = time.time()
+    SpGeMM_3D[griddim, blockdim](d_I,d_J,d_V,d_D, m, nnzperrow, max_nnz, Z)
+    t1 = time.time()
+    D = d_D.copy_to_host()
+    D_true = np.zeros((m**2*Z), dtype = np.float32)
+
+    #for i in range(Z):
+    #  _, D_true[:,:,i] = rec(I1[:,i], J1[:,i], V1[:,i], m, d)
+
+    #print(D[:,:,0])
+    #print(D_true[:,:,0])
+    cuda.synchronize()
+    er = max(er, np.linalg.norm((D-D_true).flatten()))
+    norm_rec = np.linalg.norm(D_true.flatten())
+    norm_true = np.linalg.norm(D.flatten())
+
+    del d_D
+    del d_I
+    del d_J
+    del d_V
+    del D
+    del D_true
+    del I1
+    del V1
+    del J1
+    delt1 = t1-t0
+    cuda.profile_stop()
+    
+    #tot_t += t1 + t2 + t3 + t4
+    
+    tot_t1 += delt1
+    print(l) 
+  output = ''
+  print(er)
+  print(norm_rec)
+  print(norm_true)
+  print(tot_t1)
+  output = "$\\num{%.1e}$ & $\\num{%.1e}$ & $\\num{%d}$ & $\\num{%d}$  &  $\\num{%.2e}$ & $\\num{%.3e}$ \\\ "%(nnz, m, d,Z, L,tot_t1)
+  
+
+  
+
+  #print("Invalid number of nnz per row") if nnz > 2000 else print("Number of nnz <2000")
+  
+ 
+
+  print(output)
+
+if __name__ == '__main__':
+  main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
