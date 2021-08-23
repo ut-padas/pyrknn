54bc3174a2955b0174e5c844044d332cf2e4a737
Ali Ghafouri Pourkermani <ghafouri@login4.frontera.tacc.utexas.edu>
2021-08-22 22:56:45 -0500

    fixed for iterative update of nhbds

--- GeMM/wrapper/cuda_wrapper/sparse.pyx
+++ GeMM/wrapper/cuda_wrapper/sparse.pyx
@@ -4,54 +4,52 @@
 from libcpp.vector cimport vector
 from libcpp.string cimport string
 
+import numpy as np
+cimport numpy as np
+
+import cython
+import cupy as cp
 
 cdef extern from "FIKNN_sparse.h" nogil:
   cdef void FIKNN_sparse_gpu(int *R, int *C, float *V, int *G_Id, int M, int leaves, int k, float *knn, int *knn_Id, int max_nnz);
 
 
+#def py_FIKNN_sparse(R, C, V, GId, M, leaves, k, knndis_np, knnidx_np, maxnnz):
+def py_sfiknn(gids, X, leaves, k, knndis, knnidx):
 
+  n,_ = X.shape
 
+  assert(n == len(gids))
 
-import numpy as np
-cimport numpy as np
-
-import cython
-
-
-
-
-
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-
-def py_FIKNN_sparse(R, C, V, GId, M, leaves, k, knndis_np, knnidx_np, maxnnz):
-
-  cdef int c_M = M
-  cdef int c_leaves = leaves
-  cdef int c_k = k
+  #maxnnz = cp.max(cp.diff(X.indptr))
+  maxnnz = max(np.diff(X.indptr))
   cdef int c_maxnnz = maxnnz
 
-  
-  cdef int[:] c_R 
-  cdef int[:] c_C 
-  cdef float[:] c_V
-  cdef float[:] c_knndis_np
-  cdef int[:] c_knnidx_np
-  cdef int[:] c_GId  
-   
-  c_R = np.asarray(R, dtype='int32')
-  c_C = np.asarray(C, dtype='int32')
-  c_V = np.asarray(V, dtype='float32')
-  c_GId = np.asarray(GId, dtype='int32')
+  cdef int[:] hID = gids
 
-  c_knndis_np = np.asarray(knndis_np, dtype='float32')
-  c_knnidx_np = np.asarray(knnidx_np, dtype='int32')
+  cdef float[:] data =  X.data
+  cdef int[:] idx =  X.indices
+  cdef int[:] ptr =  X.indptr
+
+  cdef int c_n = n
+  cdef int c_k = k
+  cdef int c_leaves = leaves
+
+  cdef int[:] nID = knnidx
+  cdef float[:] nDist = knndis
+
+  with nogil:
+        FIKNN_sparse_gpu(&ptr[0], &idx[0], &data[0], &hID[0], c_n, c_leaves, c_k, &nDist[0], &nID[0], c_maxnnz) 
+
+  outID = np.asarray(nID)
+  outDist = np.asarray(nDist) 
+
+  outID = np.reshape(outID, (n,k))
+  outDist = np.reshape(outDist, (n,k))
   
-  
-  FIKNN_sparse_gpu(&c_R[0], &c_C[0], &c_V[0], &c_GId[0], c_M, c_leaves, c_k, &c_knndis_np[0], &c_knnidx_np[0], c_maxnnz)
+
+  return (outID, outDist)
 
 
-  return np.asarray(c_knndis_np), np.asarray(c_knnidx_np)
+
 
