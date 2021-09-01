04d6c6e807122a402e5687794c5cc63ab44922e7
Ali Ghafouri Pourkermani <ghafouri@login1.longhorn.tacc.utexas.edu>
2021-08-31 21:38:59 -0500

    script to test sfiknn for url

---
+++ GeMM/wrapper/tree/run_url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -J KNN_URL
+#SBATCH -o logs/log_URL_9
+#SBATCH -n 1 
+#SBATCH -N 1 
+#SBATCH -t 10:00:00
+#SBATCH -p v100
+
+
+source activate gen
+
+LEVELS=9
+K=64
+MAX_ITER=300
+python url.py -use_gpu 1 -levels $LEVELS -k $K -iter $MAX_ITER
+
+conda deactivate
+
