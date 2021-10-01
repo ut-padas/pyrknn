Fixed it for single precision

GeMM/pysrc/tree/rkdtgpu.py
@@ -95,48 +95,56 @@ def rkdt_a2a_it(X,levels,knnidx,knndis,K,maxit,monitor=None,overlap=0,dense=True
     knndis : float matrix
         See above
     '''
+    begin = time.time()
+    tot_err = 0.0
+    tot_rkdt = 0.0
     n = X.shape[0]
     #perm = cp.empty_like(gids)
     #perm = cp.arange(n, dtype = cp.int32)
     
+    mempool = cp.get_default_memory_pool()
     for t in range(maxit):
         
         tic = time.time()
         #gids = np.arange(0, n,dtype=np.int32)
         gids = cp.arange(0,n,dtype=cp.int32)
-        perm = cp.arange(0, n,dtype=np.int32)
-        P,_ = ut.orthoproj(X,levels)
+        perm = cp.arange(0, n,dtype=cp.int32)
+        P = ut.orthoproj(X,levels)
         segsize = n
         for i in range(0,levels):
+            
             segsize = n>>i
-            perm = ut.segpermute_f(P[:,i],segsize,perm)
+            perm = ut.segpermute_f(P[:,i],segsize,perm)    
             P[:,:]=P[perm,:]
+            
             gids[:]=gids[perm]
+
             if 0: print(gids)
         leaves = 1 << levels
-        #pointsperleaf = int(n / leaves)
         toc = time.time() -tic
         del P
         del perm
+        print("Tree construction takes %.4f sec"%toc)
         mempool = cp.get_default_memory_pool()
-        mempool.free_all_blocks()
-        print("-"*30+ " ITERATION = %d "%t +"-"*30)
+        mempool.free_all_blocks() 
+        print("befcuda %.4f free from %.4f "%(mempool.used_bytes()/1e9, mempool.total_bytes()/1e9))
         if dense:
             dim = X.shape[1]
-            #leaf_knn(X,gids,segsize,knnidx,knndis,K,t==0,overlap)
-            #gids = np.random.permutation(np.arange(n, dtype = np.int32))
-            #gids_np[:] = gids.get()         
             py_dfiknn(gids, X, leaves, K, knnidx, knndis, dim) 
         if not dense:
-            print("\t Sparse knn : sfiknn version")
+            #print("\t Sparse knn : sfiknn version")
             py_sfiknn(gids, X, leaves, K, knndis, knnidx) 
-         
+        begin_err = time.time()
         if monitor is not None:
             if monitor(t,knnidx,knndis):
                 break
+        end_err = time.time()
+        tot_err = end_err - begin_err
         
-
-
+    end = time.time()
+    tot_rkdt = end - begin - tot_err
+    print("RKDT takes %.4f sec "%tot_rkdt)
+    print("Error takes %.4f sec "%tot_err)
 
     return knnidx, knndis
 

GeMM/pysrc/utils/utilsgpu.py
@@ -1,5 +1,6 @@
 #import numpy as np
 import cupy as cp
+import numpy as np
 import cupyx.scipy.sparse as cpsp
 
 def spnrm(X,axis):
@@ -33,17 +34,21 @@ def l2(q,r):
     return cp.sqrt(cp.abs(d)) 
 
 def orthoproj(X,numdir):
-    n,dim = X.shape
+    mempool = cp.get_default_memory_pool()
+    n,dim = X.shape 
     U = cp.random.randn(dim,numdir)
-    Q,_ =cp.linalg.qr(U,mode='reduced')
-    U[:,:Q.shape[1]] = Q
-    
-    Xr = X.dot(U)
-    #Xr = cp.dot(X,U)
-    #Xr = cp.asarray(Xr)
+    U = cp.array(U, dtype = cp.float32)
+    Q,_ = cp.linalg.qr(U,mode='reduced')
+    U[:,:Q.shape[1]] = Q 
+    Xr = cp.zeros((X.shape[0], U.shape[1]), dtype = cp.float32)
+    nvecs = U.shape[1]
+    batch = int(X.shape[0]//8)
     del Q
+    Xr[:,:] = X.dot(U)   
     
-    return (Xr,U)
+    del U
+    
+    return Xr
 
 def segpermute(arr,segsize,gperm):
     n = len(arr)