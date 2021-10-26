b49112655bd0f6fc72fe9671d6721a7645ed227e
Ali Ghafouri Pourkermani <ghafouri@c001-007.longhorn.tacc.utexas.edu>
2021-10-26 00:20:43 -0500

    seprated the headers

--- ali/include/sparse/dist/GuidedBinSearch.h
+++ ali/include/sparse/dist/GuidedBinSearch.h
@@ -6,4 +6,4 @@
 
 
 __global__ void ComputeSearchGuide(int* R_q, int* C_q, int size, int* SearchInd);
-__global__ void ComputeDists(int* R_ref, int* C_ref, float* V_ref, int* R_q, int* C_q, float* V_q, int* leafIds, float* Norms_q, float* Norms_ref, int const k_nn, float* KNN_tmp, int const ppl, int* SearchInd, int const size, int const d, int *QId, int const numqsearch);
+__global__ void ComputeDists_guided(int* R_ref, int* C_ref, float* V_ref, int* R_q, int* C_q, float* V_q, int* leafIds, float* Norms_q, float* Norms_ref, int const k_nn, float* KNN_tmp, int const ppl, int* SearchInd, int const size, int const d, int *QId, int const numqsearch);
