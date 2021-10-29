779f587391cd6848b092159f029a1099c6779984
Ali Ghafouri Pourkermani <ghafouri@login2.longhorn.tacc.utexas.edu>
2021-10-28 23:09:36 -0500

    move headers

--- ali/include/sparse/dist/Norms.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-
-__global__ void ComputeNorms(int * R, int* C, float* V, float* Norms);
-__global__ void ComputeNorms_ref(int * R, int* C, float* V, float* Norms, int* G_leafIds);
