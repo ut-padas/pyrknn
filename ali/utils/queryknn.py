c8acaf1c2b9841182dd4deb1f148250485849004
Ali Ghafouri Pourkermani <ghafouri@c001-008.longhorn.tacc.utexas.edu>
2021-10-29 15:37:53 -0500

    Added exact sparse/dense for queries

--- ali/utils/queryknn.py
+++ ali/utils/queryknn.py
@@ -4,7 +4,7 @@ import cupy as cp
 
 
 
-def queriesleafknn(X_ref, X_q, leaves, ppl, k, leafIds, q_point):
+def sparse_queriesleafknn(X_ref, X_q, leaves, ppl, k, leafIds, q_point):
 
   #qId = cp.asarray(cp.argsort(leafIds), dtype=cp.int32)
   #g_leafIds, inverse = cp.unique(leafIds[qId], return_inverse=True))
@@ -33,7 +33,67 @@ def queriesleafknn(X_ref, X_q, leaves, ppl, k, leafIds, q_point):
   return (idx, dists)
     
     
+
+
+def sparse_queriesExact(X_ref, X_q, leaves, ppl, k, q_points):
+
+  Q = X_q[q_points, :].toarray()
+  nq = Q.shape[0]
+  norm_Q = cp.tile((cp.linalg.norm(Q, axis=1)**2).reshape((nq,1)), (1, ppl))
+  
+  NDist = cp.zeros((nq, k+ppl), dtype = cp.float32) + 1e30
+  NId = cp.zeros((nq, k+ppl), dtype = cp.int32) - 1 
+ 
+  for l in range(leaves):
+    norm_ref = cp.zeros((1,ppl))
+    for p in range(ppl):
+      norm_ref[0,p] = cp.linalg.norm(X_ref.data[X_ref.indptr[l*ppl+p]:X_ref.indptr[l*ppl+p+1]])**2
+    
+    norms = norm_Q + cp.tile(norm_ref, (nq, 1))
+  
+     w = X_ref[l*ppl:(l+1)*ppl, :]
+    c_tmp = w.dot(Q.transpose())
+   
+    NDist[:, k:] = norms - 2 * c_tmp.transpose()
+    NId[:, k:] = cp.tile(cp.arange(0,ppl)+l*ppl, (nq, 1))
+   
+    idx = cp.argsort(NDist, axis=1)
+   
+    NDist[:,:] = cp.take_along_axis(NDist, idx, axis=1) 
+    NId[:,:] = cp.take_along_axis(NId, idx, axis=1) 
+  
+  return (NId[:, :k], NDist[:, :k])
+    
     
   
+def dense_queriesExact(X_ref, X_q, leaves, ppl, k, q_points):
+
+  Q = X_q[q_points, :]
+  nq = Q.shape[0]
+  norm_Q = cp.tile((cp.linalg.norm(Q, axis=1)**2).reshape((nq,1)), (1, ppl))
+  
+  NDist = cp.zeros((nq, k+ppl), dtype = cp.float32) + 1e30
+  NId = cp.zeros((nq, k+ppl), dtype = cp.int32) - 1 
+ 
+  for l in range(leaves):
+    
+    norm_ref = cp.tile( (cp.linalg.norm( X_ref[l*ppl:(l+1)*ppl, :], axis=1 )**2 ).reshape((1,ppl) ), (nq, 1))
+    
+    norms = norm_Q + cp.tile(norm_ref, (nq, 1))
+  
+     w = X_ref[l*ppl:(l+1)*ppl, :]
+    c_tmp = w.dot(Q.transpose())
+   
+    NDist[:, k:] = norms - 2 * c_tmp.transpose()
+    NId[:, k:] = cp.tile(cp.arange(0,ppl)+l*ppl, (nq, 1))
+   
+    idx = cp.argsort(NDist, axis=1)
+   
+    NDist[:,:] = cp.take_along_axis(NDist, idx, axis=1) 
+    NId[:,:] = cp.take_along_axis(NId, idx, axis=1) 
+  
+  return (NId[:, :k], NDist[:, :k])
+    
+    
    
 
